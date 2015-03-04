369e4c38afc89b49ae9357f4faa8b716f0ac708c
Matthew Halbert <matt.halbert@gmail.com>
2015-03-04 13:25:52 -0700

    class help

---
+++ assignment 4/assignment4/src/main/java/edu/asu/BMI591/halbert/Patient.java
@@ -0,0 +1,13 @@
+package edu.asu.BMI591.halbert;
+
+/**
+ * Define Patient Class
+ *
+ */
+public class Patient
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
